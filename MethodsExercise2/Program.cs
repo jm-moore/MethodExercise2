273804f1ea974784ea0eb8bc7d45bde0a5716264
jm-moore <jmmoore9358@gmail.com>
2022-08-08 11:04:20 -0500

    added additional param to Add method

--- MethodsExercise2/Program.cs
+++ MethodsExercise2/Program.cs
@@ -15,9 +15,9 @@ var band = Console.ReadLine();
 Console.WriteLine($"Once upon a time, there was a student named {name}. \n One day, {name} brought a {color} {animal} to class. \n The teacher got very upset and asked {name}" +
     $" and {name}'s pet {animal} to leave. \n {name} and {name}'s pet {animal} left class and went to a {band} concert.");
 
-static int Add(int num1, int num2)
+static int Add(int num1, int num2, int num3)
 {
-    return num1 + num2;
+    return num1 + num2 + num3;
 }
     static int Subtract(int num1, int num2)
 {
@@ -43,17 +43,21 @@ Console.WriteLine("Enter another number:");
 
 int num2 = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"If you add {num1} and {num2} you get:");
-    Console.WriteLine(Add(num1, num2));
+Console.WriteLine("Enter one last number:");
 
-Console.WriteLine($"If you subtract {num1} and {num2} you get:");
+int num3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"If you add {num1}, {num2}, and {num3} you get:");
+    Console.WriteLine(Add(num1, num2, num3));
+
+Console.WriteLine($"If you just subtract {num1} and {num2} you get:");
     Console.WriteLine(Subtract(num1, num2));
 
-Console.WriteLine($"If you multiply {num1} and {num2} you get:");    
+Console.WriteLine($"If you just multiply {num1} and {num2} you get:");    
     Console.WriteLine(Multiply(num1, num2));
 
-Console.WriteLine($"If you divide {num1} and {num2} you get;");
+Console.WriteLine($"If you just divide {num1} and {num2} you get;");
     Console.WriteLine(Divide(num1, num2));
 
-Console.WriteLine($"If you divide {num1} and {num2} you get a remainder of:");
+Console.WriteLine($"If you just divide {num1} and {num2} you get a remainder of:");
     Console.WriteLine(Modulus(num1, num2));
